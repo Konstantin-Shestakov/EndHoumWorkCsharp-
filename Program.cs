2fc2155aa831b2f9f3ea3797f6c875aed5410280
Konstantin <shestakov_k@mail.ru>
2022-08-04 17:46:49 +0300

    заполнение массивов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имющегося массива формирует массив из строк, 
+// длина которых меньше либо равно 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры 
+// либо задать на старте выполнения алгоритма.
+// [hello, 2, world, :-0] -> [2,:-)]
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Clear();
+        Console.WriteLine("Введите массив строк через пробел");
+        string[] UserWrite = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+        string[] ResultArrey = Array(UserWrite).Split(", ", StringSplitOptions.RemoveEmptyEntries);
+        
+     }
+}
